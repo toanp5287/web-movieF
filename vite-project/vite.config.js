3fca558cf611c1a77189663bd430a20dede1721d
Phạm Quang Vinh <98261160+toanp5287@users.noreply.github.com>
2026-09-25 03:01:41 +0700

    fix: fix Vercel routes

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,6 +5,8 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [
     tailwindcss(),
+
+    // Chỉ dùng khi chạy npm run dev
     {
       name: "rewrite-routes",
       configureServer(server) {
@@ -13,15 +15,23 @@ export default defineConfig({
           const path = rawUrl.replace(/\/$/, "") || "/";
 
           const routes = {
-            "/admin": "/src/admin/index.html",
+            "/": "/src/pages/home.html",
+            "/admin": "/src/admin/movies.html",
             "/movies": "/src/pages/movies.html",
             "/movie-detail": "/src/pages/movie-detail.html",
+            "/watch": "/src/pages/watch.html",
+            "/favorite": "/src/pages/favorite.html",
+            "/genres": "/src/pages/genres.html",
+            "/profile": "/src/pages/profile.html",
+            "/login": "/src/pages/login.html",
+            "/register": "/src/pages/register.html",
           };
 
           if (routes[path]) {
             const query = req.url.includes("?")
               ? req.url.slice(req.url.indexOf("?"))
               : "";
+
             req.url = routes[path] + query;
           }
 
@@ -30,13 +40,29 @@ export default defineConfig({
       },
     },
   ],
+
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        admin: resolve(__dirname, "src/admin/index.html"),
-        movies: resolve(__dirname, "src/pages/movies.html"),
-        movieDetail: resolve(__dirname, "src/pages/movie-detail.html"),
+        index: resolve(import.meta.dirname, "src/pages/home.html"),
+        admin: resolve(import.meta.dirname, "src/admin/movies.html"),
+        movies: resolve(import.meta.dirname, "src/pages/movies.html"),
+        "movie-detail": resolve(
+          import.meta.dirname,
+          "src/pages/movie-detail.html",
+        ),
+        watch: resolve(import.meta.dirname, "src/pages/watch.html"),
+        favorite: resolve(import.meta.dirname, "src/pages/favorite.html"),
+        genres: resolve(import.meta.dirname, "src/pages/genres.html"),
+        profile: resolve(import.meta.dirname, "src/pages/profile.html"),
+        login: resolve(import.meta.dirname, "src/pages/login.html"),
+        register: resolve(import.meta.dirname, "src/pages/register.html"),
+      },
+
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
       },
     },
   },
